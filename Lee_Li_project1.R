471d4b4a359fc779be705440e602f401dca33abf
mozzyl <35510608+mozzyl@users.noreply.github.com>
2020-11-13 22:04:21 -0500

    Update Lee_Li_project1.R
    Co-Authored-By: CharlesSLee <70168541+CharlesSLee@users.noreply.github.com>

--- Lee_Li_project1.R
+++ Lee_Li_project1.R
@@ -1,3 +1,22 @@
+# Note span and degree are shown with their default values.
+# degree should be 1 or 2 only
+# span can be any value in (0, 1) non-inclusive.
+
+# Your function should return a named list containing the following:
+# span: proportion of data used in each window (controls the bandwidth)
+# degree: degree of polynomial
+# N_total: total number of points in the data set
+# Win_total: total number of windows
+# n_points: number of points in each window in a vector
+# SSE: Error Sum of Squares (Tells us how good of a fit we had).
+# loessplot: An object containing the ggplot so that we can see the plot later.
+# We want this even if show.plot = FALSE
+# Note: you are NOT allowed to simply use stat_smooth() or geom_smooth() to have it automatically do LOESS.
+# You should use geom_line() or similar to plot your final the LOESS curve.
+# Make sure you can access the objects properly using the $ notation.
+
 myloess <- function(x, y, span = 0.5, degree = 1, show.plot = TRUE){
- x+y
+ if (x < 1) {
+   #
+ }
 }
